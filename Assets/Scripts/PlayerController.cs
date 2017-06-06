43f46a2cb53aaf86522084b7d9cc35c6b6e26a54
Joe Huang <happyjoe2008@gmail.com>
2017-06-06 16:09:19 -0500

    finished gaeeme

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	public float speed;
+
+	private Rigidbody playerRigidBody;
+
 	// Use this for initialization
 	void Start () {
-	
+		count = 0;
+		playerRigidBody = GetComponent<Rigidbody> ();
+		UpdateCountText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		playerRigidBody.AddForce (movement * speed);
+	}
+
+	void UpdateCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 8) {
+			winText.text = "You are winner!";
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			++count;
+			UpdateCountText ();
+		}
 	}
 }
